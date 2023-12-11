57fec3226b829e5d8a549f476cc0a82b40ff0ca9
mnlazs <mnl.azs@gmail.com>
2023-12-11 13:59:15 -0600

    updated

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-//-----------SECOND PART---------///
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
-});
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
-});
-
-// --------------FIRST PART------------------//
-
 window.addEventListener('load', obtenerDatos);
 
 // Verifica si estamos en el entorno del navegador antes de usar window
