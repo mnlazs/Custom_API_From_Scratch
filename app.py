b2ffbad46fc947f2efa607de9ac76fe838183007
NikkiMerena <alderman.nikki1022@gmail.com>
2023-12-15 11:16:49 -0600

    translated to sp/en

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 # Cargar datos desde el archivo CSV
+# Load data from the CSV file
 def cargar_datos():
     datos = []
     with open('tabla.csv', newline='') as csvfile:
@@ -16,11 +17,12 @@ def cargar_datos():
     return datos
 
 # Obtener la imagen del universo para la fecha actual desde la API de la NASA
+# Get the current day's universe image from NASA's API
 def obtener_imagen_actual():
     fecha_actual = datetime.now().strftime('%Y-%m-%d')
     return obtener_imagen(fecha_actual)
 
-
+# Fetch image from NASA's API based on a given date
 def obtener_imagen(fecha):
     api_key = 'kpzn2IRF9yVR653LJN6WPZJu7dlGZoqI8BD0gnxd'
     url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={fecha}'
@@ -29,10 +31,12 @@ def obtener_imagen(fecha):
     return data['url']
 
 # Actualizar datos del usuario en el archivo CSV
+# Update user data in the CSV file
 def actualizar_datos_csv(nombre, fecha_nacimiento, url_imagen):
     datos = cargar_datos()
 
     # Buscar si el usuario ya existe en los datos
+    # Check if the user already exists in the data
     usuario_existente = False
     for usuario in datos:
         if usuario['nombre'] == nombre and usuario['fecha_nacimiento'] == fecha_nacimiento:
@@ -40,6 +44,7 @@ def actualizar_datos_csv(nombre, fecha_nacimiento, url_imagen):
             break
 
     # Si el usuario no existe, agregarlo al archivo CSV
+    # If the user does not exist, add them to the CSV file
     if not usuario_existente:
         with open('tabla.csv', 'a', newline='') as csvfile:
             fieldnames = ['nombre', 'fecha_nacimiento', 'url_imagen']
@@ -47,6 +52,7 @@ def actualizar_datos_csv(nombre, fecha_nacimiento, url_imagen):
             writer.writerow({'nombre': nombre, 'fecha_nacimiento': fecha_nacimiento, 'url_imagen': url_imagen})
 
 # Endpoint para obtener la imagen del universo basada en la fecha de nacimiento
+# Endpoint to get the universe image based on the birth date
 @app.route('/obtener_imagen', methods=['POST'])
 def obtener_imagen_usuario():
     datos = cargar_datos()
@@ -55,23 +61,29 @@ def obtener_imagen_usuario():
 
 
     # Si no se proporciona una fecha de nacimiento, usar la fecha actual
+    # If no birth date is provided, use the current date
     if not fecha_nacimiento:
         url_imagen = obtener_imagen_actual()
     else:
     # Lógica para determinar la fecha correspondiente al día de nacimiento
+    # Logic to determine the date corresponding to the birth date
         fecha_nacimiento_dt = datetime.strptime(fecha_nacimiento, '%Y-%m-%d')
         fecha_nacimiento_str = fecha_nacimiento_dt.strftime('%Y-%m-%d')
 
         # Obtener la URL de la imagen del universo para la fecha de nacimiento
+        # Get the URL of the universe image for the birth date
         url_imagen = obtener_imagen(fecha_nacimiento_str)
 
     # Actualizar datos del usuario en el archivo CSV
+    # Update user data in the CSV file
     actualizar_datos_csv(nombre, fecha_nacimiento_str, url_imagen)
 
     # Responder con la URL de la imagen
+    # Respond with the image URL
     return jsonify({'nombre': nombre, 'url_imagen': url_imagen})
 
 # Route to serve the main page
+# Ruta para servir la página principal
 @app.route('/')
 def index():
     # Render the 'index.html' template when accessing the root '/'
