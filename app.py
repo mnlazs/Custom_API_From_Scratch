ceecd22c06ae933be4b888f9f66a359a659a34ff
daeshov <jdarahthomas@gmail.com>
2023-12-19 19:50:12 -0600

    stashing

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from flask import Flask, jsonify, request, render_template
+from flask_cors import CORS, cross_origin
 import csv
 import requests
 from datetime import datetime
 import mysql.connector
 
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/obtener_imagen": {"origins": "*"}})
+app.static_folder = 'static'
 
 def get_db_connection():
     conn = mysql.connector.connect(
@@ -16,6 +18,10 @@ def get_db_connection():
         database="Lost_in_Space"
     )
     return conn
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/tabla', methods=['POST'])
 def insert_user():
@@ -49,7 +55,7 @@ def cargar_datos():
 
 # Obtener la imagen del universo para la fecha actual desde la API de la NASA
 def obtener_imagen_actual():
-    fecha_actual = datetime.now().strftime('%m-%d-%Y')
+    fecha_actual = datetime.now().strftime('%Y-%m-%d')
     return obtener_imagen(fecha_actual)
 
 
@@ -57,11 +63,15 @@ def obtener_imagen(fecha):
     api_key = 'kpzn2IRF9yVR653LJN6WPZJu7dlGZoqI8BD0gnxd'
     url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}&date={fecha}'
     try:
+        print ('obtener imagen')
         response = requests.get(url)
         response.raise_for_status() # Esto lanzará un error si la solicitud falla
         data = response.json()
-        return data.get('url') 
-    except request.RequestException as e:
+        imagen = data.get('url')
+        print ('inside of obtener_image: ', imagen)
+        
+        return (imagen) 
+    except requests.RequestException as e:
         print(f"Error al obtener la imagen: {e}")
         return None
     
@@ -93,33 +103,37 @@ def actualizar_datos_csv(nombre, fecha_nacimiento, url_imagen):
             writer.writerow({'nombre': nombre, 'fecha_nacimiento': fecha_nacimiento, 'url_imagen': url_imagen})
 
 # Endpoint para obtener la imagen del universo basada en la fecha de nacimiento
-@app.route('/obtener_imagen', methods=['POST'])
-def obtener_imagen_usuario():
+@app.route('/obtener_imagen_actual', methods=['GET','POST'])
+@cross_origin()
+def obtener_imagen_actual():
     datos = cargar_datos()
     nombre = request.json.get('nombre')
     fecha_nacimiento = request.json.get('fecha_nacimiento')
-    
+    data = request.json
+    print(data)
+        
     if nombre is None or fecha_nacimiento is None:
     # Maneja el caso donde falta información
-        return jsonify({'error': 'Falta nombre o fecha de nacimiento'}), 400
+        response = jsonify({'error': 'Falta nombre o fecha de nacimiento'}), 400
+        return response
 
     # Si no se proporciona una fecha de nacimiento, usar la fecha actual
     if not fecha_nacimiento:
         url_imagen = obtener_imagen_actual()
     else:
     # Lógica para determinar la fecha correspondiente al día de nacimiento
-        fecha_nacimiento_dt = datetime.strptime(fecha_nacimiento, '%d-%m-%Y')
-        fecha_nacimiento_str = fecha_nacimiento_dt.strftime('%d-%m-%Y')
+        # fecha_nacimiento_dt = datetime.strptime(fecha_nacimiento, '%y-%m-%d')
+        # fecha = fecha_nacimiento_dt.strftime('%y-%m-%d')
 
         # Obtener la URL de la imagen del universo para la fecha de nacimiento
-        url_imagen = obtener_imagen(fecha_nacimiento_str)
+        url_imagen = obtener_imagen(fecha_nacimiento)
 
     # Actualizar datos del usuario en el archivo CSV
-    actualizar_datos_csv(nombre, fecha_nacimiento_str, url_imagen)
+    actualizar_datos_csv(nombre, fecha_nacimiento, url_imagen)
 
     # Responder con la URL de la imagen
     return jsonify({'nombre': nombre, 'url_imagen': url_imagen})
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True, port=8000)
