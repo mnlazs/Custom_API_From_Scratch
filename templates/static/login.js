397e6f17727f48734c074d0340701110a904e261
mnlazs <mnl.azs@gmail.com>
2023-12-21 12:33:31 -0600

    updated

---
+++ templates/static/login.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+  const errorMessage = document.getElementById('error-message');
+
+  fetch('/login', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username, password})
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Login failed');
+      }
+      return response.json();
+  })
+  .then(data => {
+      localStorage.setItem('jwtToken', data.token);
+      window.location.href = '/pagina-consulta.html';
+  })
+  .catch(error => {
+      errorMessage.textContent = error.message;
+  });
+});
